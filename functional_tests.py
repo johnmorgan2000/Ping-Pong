f14e29c5512ced1c5edf9cda91cbdd2c8d177465
basecamp <jmorgan@basecampcodingacademy.org>
2018-11-08 14:53:10 -0600

    Created test_players_inputs to check if the page gets to new page

--- functional_tests.py
+++ functional_tests.py
@@ -36,3 +36,31 @@ class TestLogin:
     #make an error message to test
 
     #make a singin user taken message test
+
+    def test_players_input(self):
+        self.browser.get("http://localhost:5500")
+
+        self.browser.find_element_by_id("loginBtn").click()
+
+        username = self.browser.find_element_by_id("username")
+        username.send_keys("Dannyp_123")
+
+        password = self.browser.find_element_by_id("password")
+        password.send_keys("Dannyp123")
+
+        self.browser.find_element_by_class_name("submitBtn").click()
+
+        player1 = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, "player_1")))
+        player2 = self.browser.find_element_by_css_selector("#player_2")
+
+        player1.send_keys("on")
+        player2.send_keys("no")
+
+        submit = self.browser.find_element_by_id("newGameBtn")
+        submit.click()
+
+        player1_name = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#player1 h3")))
+
+        assert "on" in player1_name.text
